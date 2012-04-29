fe4634e885a063114b05eadeff583f19a28ea245
Lloyd Shove <Email@lloydshove.com>
2012-04-29 10:53:07 +0800

    attempting using a trait based Trade from Java
    not there yet

--- src/main/scala/server/JavaTradeSource.java
+++ src/main/scala/server/JavaTradeSource.java
@@ -1,9 +1,11 @@
 package server;
 
 import scala.Enumeration;
+import scala.Function0;
+import scala.actors.Actor$class;
 import scala.collection.immutable.List;
-import server.model.Basket;
-import server.model.Underlying;
+import scala.runtime.BoxedUnit;
+import server.model.*;
 
 import org.specs.collection.JavaCollectionsConversion;
 
@@ -16,11 +18,58 @@ public class JavaTradeSource extends Thread{
             Underlying.HSBC()
         };
 
+
+
         List<Enumeration.Value> scalaUnderlyingsList = JavaCollectionsConversion.javaArrayToList(underlyings);
 
         TradeInputActor.$bang
                 (new TradeMessage("JavaTradeSource", "J1", new Basket("J11", scalaUnderlyingsList, 77.5)));
+
+ /*       TradeInputActor.$bang
+                (new TradeMessage("JavaTradeSource", "J1", new TradeWithKnockout("J11", scalaUnderlyingsList, 77.5)));
+
+*/
+
     }
 
+    /*class TradeWithKnockout extends Basket implements HasKnockOut, HasBarrier, BarrierListener{
+
+        private boolean breached;
+
+        public TradeWithKnockout(String id, List<Enumeration.Value> underlyings, double strikePrice) {
+            super(id, underlyings, strikePrice);
+        }
+
+        @Override
+        public void changed(double price){
+            HasKnockOut$class.changed(this, price);
+        }
+
+        @Override
+        public String description(){
+            return HasKnockOut$class.description(this);
+        }
+
+        @Override
+        public boolean breached(){
+            return this.breached;
+            //return HasBarrier$class.;
+        }
+
+        public void breached_$eq(boolean breached){
+            this.breached = breached;
+        }
+
+        @Override
+        public void act(){
+            BarrierListener$class.act(this);
+        }
+
+        @Override
+        public void onTerminate(Function0<BoxedUnit> function){
+            Actor$class.onTerminate(this, function);
+        }
+
+    }*/
 
 }
