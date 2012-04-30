07c438559f42126606c8cb6429f7f8eefddf7026
Lloyd Shove <Email@lloydshove.com>
2012-04-30 17:21:46 +0800

    cleaning up the gui text to remove that 'app' rubbish at the Lift copyright notice

--- src/main/scala/code/comet/TradeBlotter.scala
+++ src/main/scala/code/comet/TradeBlotter.scala
@@ -14,11 +14,7 @@ class TradeBlotter extends CometActor with CometListener {
   protected def registerWith = TradeActivityServer
 
   override def lowPriority = {
-<<<<<<< HEAD
-    case v : Vector[String] => trades = v; reRender();
-=======
     case v : Vector[Trade] => trades = v.map(t => t.toString); reRender();
->>>>>>> dce6f13d9b9e9ca432033910e618c0004c8742e6
   }
 
   def render = "li *" #> trades & ClearClearable
