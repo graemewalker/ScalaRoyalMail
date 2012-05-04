523864b0c5d75881e05975f447148c9eabd95174
graemewalker <graemewalker@btinternet.com>
2012-05-04 12:32:56 +0800

    RequestVar persistence across calls

--- src/main/scala/code/snippet/TradeInput.scala
+++ src/main/scala/code/snippet/TradeInput.scala
@@ -3,27 +3,42 @@ package code.snippet
 import xml.NodeSeq
 import net.liftweb.http.SHtml._
 import net.liftweb.util.Helpers._
+import net.liftweb.http._
+
+import js.JsCmds
+import js.JsCmds._
+import net.liftweb.common._
 import net.liftweb.http.SHtml
 import server.model.{Underlying, SingleTrade}
 import server.{TradeMessage, TradeInputActor}
+import net.liftweb.common.Empty
 
-class TradeInput {
+object TradeInput {
+
+  object id extends RequestVar("")
+  object strike extends RequestVar("")
+  object underlying extends RequestVar("")
 
   def add(xhtml : NodeSeq) : NodeSeq = {
-    var id = ""
-    var strike = ""
-    var underlying = ""
 
     def processEntryAdd() {
       println("Form was submitted")
 
-      TradeInputActor ! new TradeMessage("gui", "????", SingleTrade(id, Underlying.withName(underlying), 666))
+      try {
+        val strikeDouble = strike.is.toDouble
+        TradeInputActor ! TradeMessage("gui", "????", SingleTrade(id.is, Underlying.withName(underlying.is), strikeDouble))
+      }
+      catch {
+        case e: NumberFormatException => S.error("strike", "Invalid number ["+strike+"]")
+      }
     }
 
+
+    val underlyings: List[(String, String)] = Underlying.values.toList.map(ul => (ul.toString, ul.toString))
     bind("entry", xhtml,
-      "id" -> SHtml.text(id, id = _),
-      "strike" -> SHtml.text(strike, strike = _),
-      "underlying" -> SHtml.text(underlying, underlying = _),
+      "id" -> SHtml.text(id.is, id.set(_)),
+      "strike" -> SHtml.text(strike, strike.set(_)),
+      "underlying" -> SHtml.select(underlyings, Empty, underlying.set(_)),
       "submit" -> submit("Submit", processEntryAdd)
     )
   }
