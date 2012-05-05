d9cfebb3b271eec4afbebbad230667272d05fa06
graemewalker <graemewalker@btinternet.com>
2012-05-05 21:34:18 +0800

    Added 'using' control example and added call code for demo

--- src/main/scala/code/controls/Controls.scala
+++ src/main/scala/code/controls/Controls.scala
@@ -1,16 +1,33 @@
 package code.controls
 
-import server.model.Underlying._
-import server.{Tick}
-import net.liftweb.http.js.JsCmds
 
 object Controls {
 
+  /*
+   Calling code:
+   fork {
+      doSomethingComplex()
+   }
+  */
   def fork(f: Any) {
-      new Thread() {
-        override def run() {
-          f
-        }
-      }.start()
+    new Thread() {
+      override def run() {
+        f
+      }
+    }.start()
+  }
+
+  /*
+   Calling code:
+  using(myBufferedReader) {
+    reader => println(reader.readLine())
+  }
+  */
+  def using[A <: {def close() : Unit}, B](param: A)(f: A => B): B = {
+    try {
+      f(param)
+    } finally {
+      param.close()
+    }
   }
 }
